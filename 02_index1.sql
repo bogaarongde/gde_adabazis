a85d9c414f123a9c55fba50c11aa2f4d34d21ab6
Aron Boga <aron.boga@epsglobal.eu>
2023-11-04 09:27:18 +0100

    commit 5

--- 02_index1.sql
+++ 02_index1.sql
@@ -1,3 +1,18 @@
+-- karakterek generálása
+CREATE TABLE test_table (
+	id serial4 NOT NULL,
+	t int NOT NULL,
+	strtext text NOT NULL,
+	color
+	CONSTRAINT test_table_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO test_table(t, strtext)
+SELECT random() * 100, LEFT(md5(random()::text), 6)
+FROM generate_series(1, 100000);
+
+
+
 
 -- index nélküli keresés
 explain analyze SELECT * FROM test_1 where t>90;
@@ -20,8 +35,8 @@ CREATE INDEX test_table_id_name_idx ON test_table(id,name);
 
 explain select name from test_table where id = 2;
 
--- bitmap index-scan
-explain select * from test_table where name = 'ef86aad674759fb5ecffc363cc7ac8b3' or name='ef86aad674759fb5ecffc363cc7ac833';
+-- bitmap index-scan (ide be kell tenni valami lekérdezett értéket)
+explain select * from test_table where name = 'ERTEK1' or name='ERTEK2';
 
 
 -- ha nem is fut le, mert okos
